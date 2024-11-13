7dc73b1e137885a0fc5c6d5d961987c04fb769fc
3LieLs <135179431+3LieLs@users.noreply.github.com>
2024-11-13 14:20:06 -0300

    att

--- servidor.js
+++ servidor.js
@@ -28,23 +28,29 @@ if (fs.existsSync('cadastroDadosMedico.json')) {
   console.log(dados);
   vetorDadosM = JSON.parse(dados)
 }
-var vetorDadosM = []
-if (fs.existsSync('cadastroDadosMedico.json')) {
-  const dados = fs.readFileSync('cadastroDadosMedico.json', 'utf-8')
+var vetorDadosP = []
+if (fs.existsSync('cadastroDadosPaciente.json')) {
+  const dados = fs.readFileSync('cadastroDadosPaciente.json', 'utf-8')
   console.log(dados);
-  vetorDadosM = JSON.parse(dados)
-}
-var vetorDados = []
-if (fs.existsSync('cadastroDados.json')) {
-  const dados = fs.readFileSync('cadastroDados.json', 'utf-8')
-  console.log(dados);
-  vetorDados = JSON.parse(dados)
+  vetorDadosP = JSON.parse(dados)
 }
 var vetorDadosVacina = []
 if (fs.existsSync('cadastroDadosvacina.json')) {
   const dados = fs.readFileSync('cadastroDadosvacina.json', 'utf-8')
   console.log(dados);
   vetorDadosVacina = JSON.parse(dados)
+}
+var vetorDadosC = []
+if (fs.existsSync('cadastroDadosConsulta.json')) {
+  const dados = fs.readFileSync('cadastroDadosConsulta.json', 'utf-8')
+  console.log(dados);
+  vetorDadosC = JSON.parse(dados)
+}
+var vetorDados = []
+if (fs.existsSync('cadastroDados.json')) {
+  const dados = fs.readFileSync('cadastroDados.json', 'utf-8')
+  console.log(dados);
+  vetorDados = JSON.parse(dados)
 }
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -292,6 +298,89 @@ app
 
 
 
+/*-----CONSULTA----------CONSULTA----------CONSULTA----------CONSULTA----------CONSULTA----------CONSULTA----------CONSULTA----------CONSULTA----------CONSULTA----------CONSULTA-----*/
+app.get("/", (request, response) => {
+  response.render("index");
+});
+
+app.route("/cadastroC")
+  .get((req, res) => {
+    res.render("cadastroC");
+  })
+  .post(async (req, res) => {
+    let nomeForm = req.body.nomeInput
+    let idadeForm = req.body.idadeInput
+    let emailForm = req.body.emailInput
+    let telefoneForm = req.body.dddInput + '-' + req.body.celularInput
+    let CEPForm = req.body.CEPInput
+    let enderecoForm = req.body.enderecoInput
+    let vacinaForm = req.body.tipoSanguineoInput
+
+    let cadastro = {
+      'nome': nomeForm,
+      'idade': idadeForm,
+      'email': emailForm,
+      'telefone': telefoneForm,
+      'CEP': CEPForm,
+      'endereco': enderecoForm,
+      'vacina': vacinaForm,
+    }
+
+    console.log(cadastro);
+    console.log('\n' + JSON.stringify(cadastro) + ',');
+
+    vetorDadosC.push(cadastro)
+    try {
+      await client.connect();
+      await client.db("Projeto").collection("Consultas").insertOne(cadastro);
+    } finally {
+      await client.close();
+    }
+
+    res.render("consulta", { vetorDadosC });
+  });
+
+app
+  .route("/consulta")
+  .get((req, res) => {
+    res.redirect("/");
+  })
+  .post(async (req, res) => {
+    let nomeForm = req.body.nomeInput
+    let idadeForm = req.body.idadeInput
+    let emailForm = req.body.emailInput
+    let telefoneForm = req.body.dddInput + '-' + req.body.celularInput
+    let CEPForm = req.body.CEPInput
+    let enderecoForm = req.body.enderecoInput
+    let tipoSanguineoForm = req.body.tipoSanguineoInput
+
+    let cadastro = {
+      'nome': nomeForm,
+      'idade': idadeForm,
+      'email': emailForm,
+      'telefone': telefoneForm,
+      'CEP': CEPForm,
+      'endereco': enderecoForm,
+      'sangue': tipoSanguineoForm,
+    }
+
+    console.log(cadastro);
+    console.log('\n' + JSON.stringify(cadastro) + ',');
+
+    vetorDadosC.push(cadastro)
+    try {
+      await client.connect();
+      await client.db("Projeto").collection("Consultas").insertOne(cadastro);
+    } finally {
+      await client.close();
+    }
+
+    res.render("consulta", { vetorDadosC });
+  });
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 app.get("/salvar", (req, res) => {
   res.render("formulario");
 });
